49660ccbb015a4c38bcb029959a6f2ac3de5d3a9
LanceJosh <123429447+LanceJosh@users.noreply.github.com>
2023-10-18 21:23:59 +0800

    Add files via upload

---
+++ storage/framework/views/b79ccc570482da0eeed4555187934e9712eb429e.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Bacuyag First Laravel Project</title>
+</head>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="aboutme">About Me</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="sample">Sample</a>
+                </li>
+        </div>
+    </nav>
+</header>
+
+<body>
+<center>
+    <h1>Bacuyag, Lance Josh B.</h1>
+    <h1>Sample Page</h1>
+</center>
+
+</body>
+
+</html><?php /**PATH C:\Users\lance\OneDrive\Desktop\phpLaravelBacuyag\BacuyagLaravel\resources\views/samplepage.blade.php ENDPATH**/ ?>
